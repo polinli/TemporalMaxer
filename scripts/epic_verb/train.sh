440e3780d370737322a377d0bb950400349d0a10
mendelmaker <mendelmaker@gmail.com>
2023-05-27 18:10:40 +0800

    CUDA_LAUNCH_BLOCKING=1

--- scripts/epic_verb/train.sh
+++ scripts/epic_verb/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0
-python ./train.py configs/temporalmaxer_epic_slowfast_verb.yaml --save_ckpt_dir "./ckpt/verb_epic"
+CUDA_LAUNCH_BLOCKING=1 python ./train.py configs/temporalmaxer_epic_slowfast_verb.yaml --save_ckpt_dir "./ckpt/verb_epic"
